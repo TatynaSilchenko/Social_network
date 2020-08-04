b24f0914f5d6f231fa502a2d4f8228e78ec55e50
Tatyana Silchenko <taniasilchenko212@gmail.com>
2020-08-04 14:54:32 +0300

    Updates

---
+++ precache-manifest.b9948b6c406efa58f20683190841ff40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da1cbbf97dbaaec4c3d",
+    "url": "/Social_network/static/css/main.29db587b.chunk.css"
+  },
+  {
+    "revision": "4da1cbbf97dbaaec4c3d",
+    "url": "/Social_network/static/js/main.8dca3d0f.chunk.js"
+  },
+  {
+    "revision": "32da270aad77eed96330",
+    "url": "/Social_network/static/js/runtime~main.9063422f.js"
+  },
+  {
+    "revision": "8a29d85c4c6ec0a32139",
+    "url": "/Social_network/static/js/2.fd1785e1.chunk.js"
+  },
+  {
+    "revision": "757577d67e74c4faa839",
+    "url": "/Social_network/static/css/3.5203a309.chunk.css"
+  },
+  {
+    "revision": "757577d67e74c4faa839",
+    "url": "/Social_network/static/js/3.74fe0ae6.chunk.js"
+  },
+  {
+    "revision": "073d046cb3338d5662f4",
+    "url": "/Social_network/static/css/4.9f8db181.chunk.css"
+  },
+  {
+    "revision": "073d046cb3338d5662f4",
+    "url": "/Social_network/static/js/4.f42a422e.chunk.js"
+  },
+  {
+    "revision": "618c22096787a82c4ab2",
+    "url": "/Social_network/static/css/5.77f004c1.chunk.css"
+  },
+  {
+    "revision": "618c22096787a82c4ab2",
+    "url": "/Social_network/static/js/5.0780c4a3.chunk.js"
+  },
+  {
+    "revision": "63a54e17ff5b15513184f43292399e71",
+    "url": "/Social_network/static/media/preloader.63a54e17.svg"
+  },
+  {
+    "revision": "0c74cba02679ec7c39d470297cca2ef4",
+    "url": "/Social_network/index.html"
+  }
+];
